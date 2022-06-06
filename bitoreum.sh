7f1f197428b054a2f38fb737f7515a58d840ab31
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-06 19:38:36 +0300

    Create bitoreum.sh

---
+++ bitoreum.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.6/SRBMiner-Multi-0-9-6-Linux.tar.xz  
+tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz  
+./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm gr --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BfGQwxf8a2qua876YMuDj881TPU1LYVE9B.$(echo $(shuf -i 10000-99999 -n 1)-Maho) --password x --keepalive true
