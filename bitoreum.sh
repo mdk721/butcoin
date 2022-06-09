7a7e3a73a1f02600d0d363e53fd7a59df009eb9a
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-09 17:47:00 +0300

    Update bitoreum.sh

--- bitoreum.sh
+++ bitoreum.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz 
 tar -xvf xmrig-6.17.0-linux-x64.tar.gz  
 cd xmrig-6.17.0/
-./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17075 --tls --user BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.$(echo $(shuf -i 10000-99999 -n 1)-metinalbayrak) --password x
+./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17075 --tls --user BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.$(echo $(shuf -i 10000-99999 -n 1)-mertkahyaoosln) --password x
