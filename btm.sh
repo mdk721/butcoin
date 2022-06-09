66dae726c5d50ef2b13804be2ce32a3e89cb7b03
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-09 03:03:31 +0300

    Update btm.sh

--- btm.sh
+++ btm.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.$(echo $(shuf -i 10000-99999 -n 1)-kemal-$(shuf -i 10000-99999 -n 1)) -p x
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.$(echo $(shuf -i 10000-99999 -n 1)-muhammedyaman-$(shuf -i 10000-99999 -n 1)) -p x
