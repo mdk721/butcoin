79dc6054c7cc237814581035b9356daf4be72a53
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-10 04:24:25 +0300

    Update bitoreum.sh

--- bitoreum.sh
+++ bitoreum.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz 
-tar -xvf xmrig-6.17.0-linux-x64.tar.gz  
-cd xmrig-6.17.0/
-sudo ./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17075 --tls --user BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.$(echo $(shuf -i 10000-99999 -n 1)-mertkahyaoosln) --password x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.6/SRBMiner-Multi-0-9-6-Linux.tar.xz  
+tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz  
+sudo ./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.$(echo $(shuf -i 10000-99999 -n 1)-M) --password x
