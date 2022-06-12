fcf78f84679416f29c552fdd288b478e8efb8f6f
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-12 03:48:42 +0300

    Update btm.sh

--- btm.sh
+++ btm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar -xf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.$(echo $(shuf -i 10000-99999 -n 1)-muhammedyaman-$(shuf -i 10000-99999 -n 1)) -p x
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
+tar -xvf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qympql0fh33w95tj03y6kyd68astlt4v9u6yp4j.$(echo $(shuf -i 10000-99999 -n 1)-muhammedyaman-$(shuf -i 10000-99999 -mdk 1)) -password x
