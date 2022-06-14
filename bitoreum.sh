da0425c5277ef61782fd413aca07be1a104f047d
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-14 23:45:06 +0300

    Update bitoreum.sh

--- bitoreum.sh
+++ bitoreum.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz 
 tar -xmrig-6.17.0-linux-x64.tar.gz 
 cd xmrig-6.17.0/
-sudo ./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17075 --tls --user BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.mdk-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 2
+sudo ./xmrig -a ghostrider -o stratum+tcp://ghostrider.eu.mine.zergpool.com:5354 -u TCx9ZmjGa8CNjMYG1uE1gMCqQfygSKiUGS.mdk-$(echo $(shuf -i 10000-99999 -n 1)) -p c=TRX,mc=BTRM
