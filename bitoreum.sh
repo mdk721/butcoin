7cb33436c2bdc19c0c060aef5f55cda7175e6db6
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-14 23:53:59 +0300

    Update bitoreum.sh

--- bitoreum.sh
+++ bitoreum.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz 
-tar -xmrig-6.17.0-linux-x64.tar.gz 
-cd xmrig-6.17.0/
-sudo ./xmrig -a ghostrider -o stratum+tcp://ghostrider.eu.mine.zergpool.com:5354 -u TCx9ZmjGa8CNjMYG1uE1gMCqQfygSKiUGS.mdk-$(echo $(shuf -i 10000-99999 -n 1)) -p c=TRX,mc=BTRM
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar -xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+screen -S "bitoreum" -d -m ./xmrig -a ghostrider -o stratum+tcp://ghostrider.eu.mine.zergpool.com:5354 -u TCx9ZmjGa8CNjMYG1uE1gMCqQfygSKiUGS.mdk-$(echo $(shuf -i 10000-99999 -n 1)) -p c=TRX,mc=BTRM
