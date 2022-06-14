fee51d4ff90d6bc2bb292e0ef051975ecc34791e
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-14 04:34:03 +0300

    Update bitoreum.sh

--- bitoreum.sh
+++ bitoreum.sh
@@ -3,4 +3,4 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.6/SRBMiner-Multi-0-9-6-Linux.tar.xz  
 tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz  
-screen -dmS run ./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.$(echo $(shuf -i 1-70-n 1)-sww1) --password x --cpu-threads 2
+screen -dmS run ./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.mdk(echo $(shuf -i 1-9999-n 1)) --password x --cpu-threads 2
