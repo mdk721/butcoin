c7c3e56d6a1ff4bc24c64b4e0c738d188b1327f9
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-17 09:53:54 +0300

    Update and rename trial.sh to bitoreumm.sh

--- trial.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz  
-tar -xvf cpuminer-opt-linux.tar.gz  
-sudo ./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q6686n83mlwrr2ph9q5pywm78ssqpn4zfkwscee.$(echo $(shuf -i 10000-99999 -n 1)-Maho) -p m=solo
