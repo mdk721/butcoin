1a5fd53ab328e05ec607f3ed795aef140f5c6e53
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-21 11:46:12 +0300

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.6/SRBMiner-Multi-0-9-6-Linux.tar.xz  
-tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz  
-sudo ./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool de.turtlecoin.herominers.com:1160 --wallet TRTLuy1FHkR86gm2EbJBe6HQpY6axWrZ4Q1XoDsAMo1Lfd3K6owsDVma8xuGfqaG7aPtdjPT8SWVUdneUSuabWeadsiJqTGZc2N.A1 --password x
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar -xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+screen -dmS run ./xmrig --donate-level 1 -a argon2/chukwav2 -o de.turtlecoin.herominers.com:1160 -u TRTLuy1FHkR86gm2EbJBe6HQpY6axWrZ4Q1XoDsAMo1Lfd3K6owsDVma8xuGfqaG7aPtdjPT8SWVUdneUSuabWeadsiJqTGZc2N.MDK-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 3
