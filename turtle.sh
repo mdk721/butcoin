262745536a999a9050fd47799261f4711687b22d
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-07-13 14:30:32 +0300

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar -xf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-screen -dmS run ./xmrig --donate-level 1 -a argon2/chukwav2 -o de.turtlecoin.herominers.com:1160 -u TRTLuy1FHkR86gm2EbJBe6HQpY6axWrZ4Q1XoDsAMo1Lfd3K6owsDVma8xuGfqaG7aPtdjPT8SWVUdneUSuabWeadsiJqTGZc2N.MDK-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 3
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+tar -xf xmrig-6.18.0-linux-x64.tar.gz
+cd xmrig-6.18.0
+screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BVdAKt5opzf6tDqAzyZqgb87VXdvKnwchV.MDK-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 8
