472b34937dca847a1b60c8a41f3a1940e09c9d74
Muhammed Diyadin Karabulut <68358195+mdk721@users.noreply.github.com>
2022-09-14 20:13:58 +0300

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sleep 15
-sudo apt update
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.7/SRBMiner-Multi-1-0-7-Linux.tar.xz
-tar -xf SRBMiner-Multi-1-0-7-Linux.tar.xz
-sudo ./srbminer-multi --algorithm gr --pool stratum-eu.rplant.xyz:17075 --tls true --wallet Bj1GnWYL5sprwLJAt9Ep7dMh3z9p8JA9gg.mdk --keepalive true
+tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz 
+sudo ./SRBMiner-Multi-1-0-7/SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet Bj1GnWYL5sprwLJAt9Ep7dMh3z9p8JA9gg.mdk --keepalive true
