0df57e087313f30d7265cd34fb7a504f57928afa
Muhammed Diyadin Karabulut <68358195+mdk721@users.noreply.github.com>
2022-09-15 11:46:45 +0300

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sudo apt update
-sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u Bj1GnWYL5sprwLJAt9Ep7dMh3z9p8JA9gg.MDK-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 8
+./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u Bj1GnWYL5sprwLJAt9Ep7dMh3z9p8JA9gg.MDK-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 8
